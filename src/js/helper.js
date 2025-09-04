aa5d18b3f8a5b521de0999de15ed761c6b349bf2
pro <pro>
2025-09-04 23:18:12 +0530

    first commit

---
+++ src/js/helper.js
@@ -0,0 +1,24 @@
+import { timeout_sec } from "./config"
+const timeout=function(sec)
+{
+    return new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            reject(new Error(`Api took more then ${sec} second to respond`))
+        },sec*1000)
+    })
+}
+export const getJSON=async function(url)
+{
+    try{
+    const data=await Promise.race([fetch(url),timeout(timeout_sec)])
+    const res=await data.json();
+    if(!data.ok)
+    throw new Error(res.message)
+    return res;
+    }
+    catch(err)
+    {
+        console.log("pro")
+        throw err
+    }
+}
